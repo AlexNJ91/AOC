c25cc4e6819f8ab60403ae484500c0aaefcad369
Alex Johnston <alexnjohnston25@gmail.com>
2021-12-01 14:50:09 +0000

    AOC Day 1

---
+++ Day_1/aoc_day1_solution.rb
@@ -0,0 +1,6 @@
+puzzle = File.read("aoc_day1.txt").lines.map(&:to_i)
+
+# Part 1
+p puzzle.each_cons(2).count { |a,b| b > a }
+# Part 2
+p puzzle.each_cons(3).map(&:sum).each_cons(2).count { |a,b| b > a }
