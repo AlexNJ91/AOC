c8fbc4b0c6c8bee7ee054a366fa0f9b58afe66ee
Alex Johnston <alexnjohnston25@gmail.com>
2022-02-16 20:41:30 +0000

    day 5

---
+++ Day_5/aoc_day5.rb
@@ -0,0 +1,33 @@
+require_relative "./aoc_day5_matrix.rb"
+
+file = File.open("aoc_day5.txt")
+file_data = file.read
+list = file_data.split("\n")
+
+matrix = Matrix.new(1000, 1000)
+
+# PART 1
+
+list.each do |line|
+    first = line.split(" -> ")[0].split(',')
+    second = line.split(" -> ")[1].split(',')
+
+    first = Point.new(first[0].to_i, first[1].to_i)
+    second = Point.new(second[0].to_i, second[1].to_i)
+    matrix.mark_horizontal_line(first, second)
+end
+
+p matrix.count_2s
+
+# PART 2
+
+list.each do |line|
+    first = line.split(" -> ")[0].split(',')
+    second = line.split(" -> ")[1].split(',')
+
+    first = Point.new(first[0].to_i, first[1].to_i)
+    second = Point.new(second[0].to_i, second[1].to_i)
+    matrix.mark_vertical_line(first, second)
+end
+
+p matrix.count_2s
